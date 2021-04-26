c8029f0237b4bebaa7bbb6a8a82f9a2b874b3e5c
Sandu <sandurares57@gmail.com>
2021-04-26 10:05:00 +0300

    kata 26.04

--- morningkatas.js
+++ morningkatas.js
@@ -103,3 +103,61 @@ const snail = (arr) => {
   }
   return finalArray
 }
+
+/* 
+Kata - 26.04.2021
+Backspaces in string
+
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+Your task is to process a string with "#" symbols.
+
+Examples
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+*/
+function cleanString(s) {
+	let arr = []
+  
+  s.split('').forEach(char => {
+    if(char === '#'){
+      arr.pop()
+    } else {
+    arr.push(char)
+    }
+  })
+  
+  return arr.join('');
+};
+
+/* 
+Most profit from stock quotes
+
+ Return the most profit from stock quotes.
+
+ Stock quotes are stored in an array in order of date. The stock profit is the difference in prices in buying and selling stock. Each day, you can either buy one unit of stock, sell any number of stock units you have already bought, or do nothing. Therefore, the most profit is the maximum difference of all pairs in a sequence of stock prices.
+
+@param {array} quotes
+@return {number} max profit
+Example:
+
+ [ 1, 2, 3, 4, 5, 6]   => 15  (buy at 1,2,3,4,5 and then sell all at 6)
+ [ 6, 5, 4, 3, 2, 1]   => 0   (nothing to buy)
+ [ 1, 6, 5, 10, 8, 7 ] => 18  (buy at 1,6,5 and sell all at 10)
+
+*/
+
+function getMostProfitFromStockQuotes(quotes) {
+  let max = Math.max(...quotes)
+  let i = 0;
+  let profit = 0;
+  
+   while(quotes[i]<max){
+     profit = profit + (max - quotes[i])
+     i++;
+   }
+  
+  return profit
+}
