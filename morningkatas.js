d55cd0b90d1e28576f97794e25a365bca9a1189e
Sandu <sandurares57@gmail.com>
2021-05-17 10:07:12 +0300

    kata 17.05.2021

--- morningkatas.js
+++ morningkatas.js
@@ -971,3 +971,35 @@ PaginationHelper.prototype.pageIndex = function(itemIndex) {
   }
   return Math.floor(itemIndex / this.itemsPerPage);
 }
+
+/* 
+kata 17.05.2021
+
+
+*/
+
+// one liners
+
+function PaginationHelper(collection, itemsPerPage){
+  this.collection = collection, this.itemsPerPage = itemsPerPage;
+}
+
+PaginationHelper.prototype.itemCount = function() {
+  return this.collection.length;
+}
+
+PaginationHelper.prototype.pageCount = function() {
+  return Math.ceil(this.collection.length / this.itemsPerPage);
+}
+
+PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+  return pageIndex < this.pageCount() 
+    ? Math.min(this.itemsPerPage, this.collection.length - pageIndex * this.itemsPerPage)
+    : -1;
+}
+
+PaginationHelper.prototype.pageIndex = function(itemIndex) {
+  return itemIndex < this.collection.length && itemIndex >= 0
+    ? Math.floor(itemIndex / this.itemsPerPage)
+    : -1;
+}
