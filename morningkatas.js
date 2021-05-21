55a322780ad9dedccdfdf936c91adcb1c1cf744a
Sandu <sandurares57@gmail.com>
2021-05-21 10:02:35 +0300

    kata 21.05.2021

--- morningkatas.js
+++ morningkatas.js
@@ -1075,3 +1075,29 @@ function convert(s) {
 
     return parseInt(result.join(''), 10)
 }
+
+/* kata 20.05.2021
+https://www.codewars.com/kata/58f8a3a27a5c28d92e000144
+*/
+function firstNonConsecutive (arr) {
+  
+  for(var i=0; i <= arr.length; i++ ){
+    if( ( arr[i + 1] - arr[i] ) > 1 || arr[i+1] -arr[i] < -1){
+      return arr[i+1]
+    }
+    
+  }
+  return null
+}
+
+/* kata 21.05.2021
+
+https://www.codewars.com/kata/57cc981a58da9e302a000214/train/javascript
+*/
+function smallEnough(a, limit){
+  let filterResult = a.filter(num => num <= limit)
+  
+ return filterResult.length < a.length ?   false : true
+  
+  
+}
