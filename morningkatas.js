08261e9a1c811569a48a80194974b9c0ed0c43f2
Sandu <sandurares57@gmail.com>
2021-05-24 10:17:09 +0300

    kata 24.05.2021

--- morningkatas.js
+++ morningkatas.js
@@ -1101,3 +1101,42 @@ function smallEnough(a, limit){
   
   
 }
+
+/* 
+kata 24.05.2021
+https://www.codewars.com/kata/609eee71109f860006c377d1/train/javascript*/
+
+function lastSurvivor(letters, coords) {
+  let arr = letters.split('')
+coords.forEach(cor => {
+ return arr.splice(cor, 1)
+})
+  return arr.join('') 
+}
+
+/* 
+
+https://www.codewars.com/kata/56e3cd1d93c3d940e50006a4/train/javascript
+*/
+
+function makeValley(arr) {
+  let result = [];
+  let a = arr.sort((a,b) => {return a - b;})  
+  
+  a.map((num,i) => {    
+    if(i%2 === 0){
+    result.push(num)
+    } else {
+      result.unshift(num)
+    }
+  })
+   
+  if(a.length % 2 === 0) {
+  return result
+  } else {
+  return result.reverse()   
+  }
+}
+
+const makeValley = arr =>
+  [...arr.sort((a, b) => b - a).filter((_, idx) => !(idx % 2)), ...arr.filter((_, idx) => idx % 2).reverse()];
