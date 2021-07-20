86e173c1e849da88c6399fe55f918187c8319052
Sandu <sandurares57@gmail.com>
2021-07-20 10:23:30 +0300

    kata 20.07.2021

---
+++ katas/Persistent-Bugger.js
@@ -0,0 +1,53 @@
+/* 
+
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example:
+
+ persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+                       // and 4 has only one digit
+                 
+ persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+                        // 1*2*6 = 12, and finally 1*2 = 2
+                  
+ persistence(4) === 0 // because 4 is already a one-digit number
+
+ https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/javascript
+*/
+
+function persistence(num, count = 1) {
+  //code me
+  if (num.toString().length === 1) {
+    return 0;
+  }
+
+  let splitedNums = num
+    .toString()
+    .split('')
+    .reduce((acc, curr) => {
+      return parseInt(acc) * parseInt(curr);
+    });
+  if (splitedNums.toString().length === 1) {
+    return count;
+  } else {
+    count++;
+    return persistence(splitedNums, count);
+  }
+}
+
+function persistence(num) {
+  var times = 0;
+
+  num = num.toString();
+
+  while (num.length > 1) {
+    times++;
+    num = num
+      .split('')
+      .map(Number)
+      .reduce((a, b) => a * b)
+      .toString();
+  }
+
+  return times;
+}
